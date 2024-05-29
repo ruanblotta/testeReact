9e005296f56969039722168ae68bca880d160396
Ruan Blotta <ruanblotta@gmail.com>
2024-05-28 21:39:40 -0300

    Função incrementar e decrementr

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
- 
+import React, { useState } from 'react';
+
 function App() {
- 
+  const [contador, setContaor] = useState(0);
+
+  const incrementar = () => {
+    setContator(contador + 1);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  }
+
   return (
-    <>
-      <h1>Ruan Blotta</h1>
-    </>
-  )
+    <div>
+      <p>Você clicou {contador} vezes</p>
+      <button onClick={incrementar}>+</button>
+      <button onClick={decrementar}>-</button>
+    </div>
+  );
+
+  function App() {
+    const nome = "Ruan";
+    const sobrenome = "Marques";
+
+    return (
+      <div>
+        <p>{nome} {sobrenome}</p>
+      </div>
+    );
+  }
 }
- 
-export default App
- 
+export default App;
