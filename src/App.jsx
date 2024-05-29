e5aa2e971bda2cc60afb702c7ed81886ff84773f
Ruan Blotta <ruanblotta@gmail.com>
2024-05-28 21:42:01 -0300

    Função Nome/Sobrenome utilizando const

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [contador, setContaor] = useState(0);
-
-  const incrementar = () => {
-    setContator(contador + 1);
-  };
-
-  const decrementar = () => {
-    setContador(contador - 1);
-  }
-
-  return (
-    <div>
-      <p>Você clicou {contador} vezes</p>
-      <button onClick={incrementar}>+</button>
-      <button onClick={decrementar}>-</button>
-    </div>
-  );
-
-  function App() {
     const nome = "Ruan";
-    const sobrenome = "Marques";
+    const sobrenome = "Blotta";
 
     return (
       <div>
         <p>{nome} {sobrenome}</p>
       </div>
     );
-  }
 }
 export default App;
