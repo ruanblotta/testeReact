bd252bb481c1c315269134455e2ad63b9c3dc85c
Ruan Blotta <ruanblotta@gmail.com>
2024-06-05 18:49:16 -0300

    Atividade Array de Objetos

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import './App.css';
 
 function App() {
   
-  const busca = "futebol";
+  // const busca = "futebol";
+  let series = new Array("Game of Thrones", "House of the Dragon", "Dark", "Fallout", "The Lord of the Rings: The Rings of Power");
   
   return (
     <div>
-        <h1 className="title">Site de Buscas</h1>
-        <a href={`https://google.com/search?q=${busca}`} title="Buscador" >
+        <h1 className="title">Séries</h1>
+        <ul>
+          {series.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        {/* <a href={`https://google.com/search?q=${busca}`} title="Buscador" >
           Buscador
-        </a>
+        </a> */}
     </div>
   );
 }
