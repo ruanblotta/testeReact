c902e85d4deaff27361d2a001e4d6d5a23eaf986
Ruan Blotta <ruanblotta@gmail.com>
2024-06-04 21:49:55 -0300

    Estudo em React

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    let name = "Ruan";
-    let num1 = 1;
-    let num2 = 1;
-    let sum = num1 + num2;
-    return (
-      <div>
-        <p>Meu nome é <span>{name}</span></p>
-        <p>A soma é {sum}</p>
-      </div>
-      
-    );
+  
+  const busca = "futebol";
+  
+  return (
+    <div>
+        <h1 className="title">Site de Buscas</h1>
+        <a href={`https://google.com/search?q=${busca}`} title="Buscador" >
+          Buscador
+        </a>
+    </div>
+  );
 }
 export default App;
+
