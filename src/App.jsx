5c89b29cb6023418ef1ee8514b5856ad4ecb6f2a
Ruan Blotta <ruanblotta@gmail.com>
2024-06-11 21:32:59 -0300

    Update - Prática em aula

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   
-  // const busca = "futebol";
-  let series = new Array("Game of Thrones", "House of the Dragon", "Dark", "Fallout", "The Lord of the Rings: The Rings of Power");
-  
   return (
-    <div>
-        <h1 className="title">Séries</h1>
-        <ul>
-          {series.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        {/* <a href={`https://google.com/search?q=${busca}`} title="Buscador" >
-          Buscador
-        </a> */}
-    </div>
-  );
+
+   <div className='App'>
+      <h1>Magic the Gathering</h1>
+      <img src="https://cards.scryfall.io/large/front/1/b/1b0f41e8-cf27-489b-812a-d566a75cf7f7.jpg?1559596849" alt="Basic Mountain" width="300" />
+      <p>Basic Mountain.</p>
+      <p>Artist: Douglas Shuler</p>
+      <p>Revised Edition </p>
+      <p>Card number 301</p>
+      <button onClick={() => alert("Tap this Basic Mountain")}>Check for more info!</button>
+   </div>
+  )
 }
 export default App;
 
