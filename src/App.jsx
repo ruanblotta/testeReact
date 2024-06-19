9e1bbeddfb56ba4eeee7b607ada2e761bb212c99
Ruan Blotta <ruanblotta@gmail.com>
2024-06-18 21:54:46 -0300

    Implementando adicionar e remover informações

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  
-  return (
+const initialData = [
+  { id: 1, nome: "Ruan Blotta", endereco: "Av xpto, 10", numero: "00000000", cpf: "00000000000", email: "email@email.com", telefone: "1199999999" },
+  { id: 2, nome: "Elvis Presley", endereco: "Av xpto, 10", numero: "00000000", cpf: "00000000000", email: "email@email.com", telefone: "1199999999" },
+  { id: 3, nome: "Ariana Grande", endereco: "Av xpto, 10", numero: "00000000", cpf: "00000000000", email: "email@email.com", telefone: "1199999999" },
+  { id: 4, nome: "Ayrton Senna", endereco: "Av xpto, 10", numero: "00000000", cpf: "00000000000", email: "email@email.com", telefone: "1199999999" }
+];
 
-   <div className='App'>
-      <h1>Magic the Gathering</h1>
-      <img src="https://cards.scryfall.io/large/front/1/b/1b0f41e8-cf27-489b-812a-d566a75cf7f7.jpg?1559596849" alt="Basic Mountain" width="300" />
-      <p>Basic Mountain.</p>
-      <p>Artist: Douglas Shuler</p>
-      <p>Revised Edition </p>
-      <p>Card number 301</p>
-      <button onClick={() => alert("Tap this Basic Mountain")}>Check for more info!</button>
-   </div>
+function App() {
+
+  const [cadastros, setCadastros] = useState(initialData);
+
+  const handleDeleteClick = (id) => {
+    setCadastros(cadastros.filter(cadastro => cadastro.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Lista de Cadastros</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Número</th>
+            <th>CPF</th>
+            <th>Email</th>
+            <th>Telefone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cadastros.map((cadastro) => (
+            <tr key={cadastro.id}>
+              <td>{cadastro.id}</td>
+              <td>{cadastro.nome}</td>
+              <td>{cadastro.endereco}</td>
+              <td>{cadastro.numero}</td>
+              <td>{cadastro.cpf}</td>
+              <td>{cadastro.email}</td>
+              <td>{cadastro.telefone}</td>
+              <td>
+                <button onClick={() => handleDeleteClick(cadastro.id)} className="red">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 export default App;
